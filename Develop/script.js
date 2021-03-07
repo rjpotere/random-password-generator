4815e72e9c854f269735a3b393825025c6527bac
Potears <ryan.potere@gmail.com>
2021-03-07 00:46:58 -0500

    generate password prompt opens with a condition to add a number between 8 and 128.

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,22 @@ function writePassword() {
 
 }
 
+//create generate password function
+
+function generatePassword(){
+  var length = window.prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
+
+  if (length == null) {
+    return;
+
+  } else if (length >= 8 && length <= 128) {
+    // run next step
+
+  } else {
+    window.alert("Please input a number between 8 and 128.")
+    return;
+  }
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
