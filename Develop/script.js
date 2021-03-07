6c14cb1ac97d998999a17a3df825490c69454591
Potears <ryan.potere@gmail.com>
2021-03-07 01:30:21 -0500

    variables added for password and confirm windows added for password structure.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upper = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+upper = upper.toLocaleUpperCase;
+var lower = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+var number = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+var symbol = ("!", "@", "#", "$", "%", "^", "&", "*", "/", "?");
 
 // Write password to the #password input
 function writePassword() {
@@ -25,6 +30,13 @@ function generatePassword(){
     window.alert("Please input a number between 8 and 128.")
     return;
   }
+
+  var upperMessage = window.confirm("Would you like to use uppercase letters?");
+  var lowerMessage = window.confirm("Would you like to use lowercase letters?");
+  var numberMessage = window.confirm("Would you like to use numbers?");
+  var symbolMessage = window.confirm("Would you like to use special characters?");
+
+
 }
 
 // Add event listener to generate button
