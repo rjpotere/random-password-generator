786bd3cfe346c506b387009225620560cd3737ad
Potears <ryan.potere@gmail.com>
2021-03-07 14:51:02 -0500

    password logged onto screen.

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upper = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-upper = upper.toLocaleUpperCase;
-var lower = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-var number = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-var symbol = ("!", "@", "#", "$", "%", "^", "&", "*", "/", "?");
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+
 
 // Write password to the #password input
 function writePassword() {
@@ -36,8 +36,33 @@ function generatePassword(){
   var numberMessage = window.confirm("Would you like to use numbers?");
   var symbolMessage = window.confirm("Would you like to use special characters?");
 
+  var randomPassword = [];
 
-}
+  if (upperMessage) {
+    randomPassword = randomPassword.concat(upper);
+  }
+
+  if (lowerMessage) {
+    randomPassword = randomPassword.concat(lower);
+  }
+
+  if (numberMessage) {
+    randomPassword = randomPassword.concat(number);
+  }
+
+  if (symbolMessage) {
+    randomPassword = randomPassword.concat(symbol);
+  }
+
+  var securePassword = " ";
+
+  for (i=0; i < length; i++) {
+    securePassword = securePassword + randomPassword [Math.floor(Math.random() * randomPassword.length)];
+  }
+
+
+ return securePassword;
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
